f6104bde29a79c508ce93b806bac5acdc371a844
lone-wolffie <karenwanjiru09@gmail.com>
2025-09-30 00:19:32 +0300

    Modify post routes and hashing

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-
 const db = new pg.Client({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
@@ -19,77 +18,81 @@ const db = new pg.Client({
 });
 db.connect();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static("public"));
 
-//inserting user info and hashed password to db when signing up
-async function passwordHashing(originalPassword) {
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(originalPassword, saltRounds);
-
-    await db.query(
-        "INSERT INTO signup (full_name, email_address, phone_number, username, password) VALUES ($1, $2, $3, $4, $5)",
-        [full_name, email_address, phone_number, username, hashedPassword]
-    );
-}
-
+//Homepage
 app.get("/" , async (req, res) => {
     try {
-        res.sendFile("home.html", {root: "public"});
+        res.sendFile("index.html", { root: "public" });
     } catch(err) {
         console.log(err);
     }
 });
+
+//inserting user info and hashed password to db when signing up
+app.post("/signup", async (req, res) => {
+    try {
+        const { fullname, email, phonenumber, username, password } = req.body;
+
+        if (!fullname || !email || !phonenumber || !username || !password) {
+            return res.json({Error: "All fields are required"});
+        }
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        await db.query(
+        "INSERT INTO signup (full_name, email_address, phone_number, username, password) VALUES ($1, $2, $3, $4, $5)",
+        [
+            req.body.fullname, 
+            req.body.email, 
+            req.body.phonenumber, 
+            req.body.username, 
+            hashedPassword
+        ]
+        );
+        res.redirect("/home.html");
+    
+    } catch(err) {
+        console.log("Sign Up error:", err);
+        res.json({Error: "Sign Up failed. Please try again later."});
+
+    }
+});
+
 app.post("/login", async(req, res) => {
-    try{
+    try {
         const { username, password } = req.body;
+
         const result = await db.query(
-            "SELECT password FROM signup WHERE username = $1",
+            "SELECT username, password FROM signup WHERE username = $1", 
             [username]
         );
+        // console.log(result);
 
         if(result.rows.length === 0) {
             return res.json({Error: "User not found"});
         }
-        // hashed password stored in db
+        // compare hashed password stored in db to user password entered
         const storedHashedPassword = result.rows[0].password;
         const matchedPassword = await bcrypt.compare(password, storedHashedPassword);
 
         if (matchedPassword) {
-            return res.json({message: "Login successful"});
+            //res.json({message: "Login successful."});
+            return res.redirect("/home.html");
         } else {
-            return res.json({message: "Invalid password."})
+            //res.json({message: "Invalid password."});
+            return res.send("Invalid password");
         }
 
-    } catch (err) {
+    } catch(err) {
         console.log("Login error:", err);
-        res.json({error: "Login failed. Please try again later."})
-    };
-});
-
-app.post("/signup", async (req, res) => {
-    try {
-        const result = await db.query(
-            "SELECT username, password FROM signup", 
-            [username, password]
-        );
-
-        if (result) {
-            res.redirect("/");
-        } else {
-            res.redirect("/signup");
-        }
-        
-    } catch (err) {
-        console.log("Sign Up error:", err);
-        res.json({Error: "Sign Up failed. Please try again later."})
+        return res.json({error: "Login failed. Please try again later."});
     }
 });
-
-
-
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
